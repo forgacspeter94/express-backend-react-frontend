56de9faf8e60bd1fcf169b6f76674f0f0be68c31
forgacspeter94 <forgacspeter940@gmail.com>
2022-10-07 22:16:40 +0200

    addTodo started

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+const path = require ('path');
+
+const app = express();
+
+
+app.get ('/todos', (req, res) => res.sendFile(path.join(`${__dirname}/todos.json`)));
+
+app.listen (9000, (req, res) =>{
+    console.log('server is running at http://127.0.0.1:9000')
+})
